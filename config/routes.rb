fecbbb151df46a612af5ad42b06e2ad0515b3ac4
AJ-Kulundu <jameskulundu@gmail.com>
2023-03-24 12:26:25 +0300

    Fixing the questions resource route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :questions, only:[:index,:create]
+      resources :questions
     end
   end
 end
